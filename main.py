3e7ad2c300aadd592cffcf0bd309a430d98a84b0
Alset-N <34602864+ukaszogi@users.noreply.github.com>
2020-02-15 22:50:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,36 +8,36 @@ sudoku = [
     [4,3,0,0,0,0,0,0,6],
     [0,0,6,0,9,0,8,0,0],
     [0,0,0,0,0,5,0,0,3]
-]
+] #0 is for numbers, that we want to solve for 
 
-def pisz(a):
+def pisz(a):  #it's basically some better way to print-out 2d-list
     for i in a:
         print(i)
 
-def test(y,x,n):
+def test(y,x,n):  #this func tests if given n can be placed in x-th row and y-th collumn
     global sudoku
     for i in range(9):
-        if (sudoku[y][i]==n) | (sudoku[i][x]==n):
+        if (sudoku[y][i]==n) | (sudoku[i][x]==n):  #checks for any n istances in row and in column
             return False
-    y0 = (y//3)*3
+    y0 = (y//3)*3  #this checks in wich 3x3 sqare (x,y) is
     x0 = (x//3)*3
     for i in range(3):
         for j in range(3):
-            if sudoku[y0+i][x0+j]==n:
+            if sudoku[y0+i][x0+j]==n:  #checks for any n istances in 3x3
                 return False
     return True
 
-def doo():
+def do():
     global sudoku
     for i in range(9):
         for j in range(9):
-            if sudoku[i][j]==0:
+            if sudoku[i][j]==0: #if there won't be any 0 places left, do will just pisz()-out the solucion 
                 for n in range(1,10):
                     if test(i,j,n):
                         sudoku[i][j]=n
-                        doo()
+                        do()  #it's basically some backpropagation - program just accepts the first value and checks, if you can solve now = if the value is correct
                         sudoku[i][j]=0
                 return
     pisz(sudoku)
     input("Next")
-doo()
+do()
