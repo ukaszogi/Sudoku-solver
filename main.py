059ac74be31e1fcdefc5d977547d1f1332cdeb77
Alset-N <34602864+ukaszogi@users.noreply.github.com>
2020-02-15 22:28:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,29 +17,26 @@ def pisz(a):
 def test(y,x,n):
     global grid
     for i in range(9):
-        if grid[y][i]==n:
-            return False
-    for i in range(9):
-        if grid[i][x]==n:
+        if (grid[y][i]==n) | (grid[i][x]==n):
             return False
     y0 = (y//3)*3
     x0 = (x//3)*3
-    for i in range(0,3):
-        for j in range(0,3):
+    for i in range(3):
+        for j in range(3):
             if grid[y0+i][x0+j]==n:
                 return False
     return True
 
 def solve():
     global grid
-    for y in range(9):
-        for x in range(9):
-            if grid[y][x]==0:
+    for i in range(9):
+        for j in range(9):
+            if grid[i][j]==0:
                 for n in range(1,10):
-                    if test(y,x,n):
-                        grid[y][x]=n
+                    if test(i,j,n):
+                        grid[i][j]=n
                         solve()
-                        grid[y][x]=0
+                        grid[i][j]=0
                 return
     pisz(grid)
     input("Next")
